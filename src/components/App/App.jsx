51b5a210e35b2ec09f45618e47199b2b8700b472
Walker Neudorff <walkerneudorff@gmail.com>
2023-11-27 18:06:58 -0600

    Implemented router and routes in App jsx, succesfully links to each page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import AboutPage from '../AboutPage/AboutPage.jsx';
 
 function App() {
@@ -13,10 +13,27 @@ function App() {
             </header>
             <br/>
             <Router>
+            <ul>
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+                <Link to="/allStudents">
+                    <li>All Students</li>
+                </Link>
+            </ul>
+            <Route exact path="/">
             <StudentForm />
+            </Route>
+            <Route exact path="/allStudents">
             <p>Student list:</p>
             <StudentList />
+            </Route>
+            <Route exact path="/about">
             <AboutPage />
+            </Route>
             </Router>
         </div>
     );
